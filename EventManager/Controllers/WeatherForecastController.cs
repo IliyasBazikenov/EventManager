706e60c25f434a3dc52a9f2a772733e8df416437
Iliyas Bazikenov <i.bazikenov@gmail.com>
2019-11-19 21:59:41 +0600

    second commit

--- EventManager/Controllers/WeatherForecastController.cs
+++ EventManager/Controllers/WeatherForecastController.cs
@@ -27,6 +27,7 @@ namespace EventManager.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
+            var rng2 = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
